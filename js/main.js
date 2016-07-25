8906af098913f87fdbebe73eeca3136b92e727a3
Matthias Gallee <gallee.matthias@gmail.com>
2016-07-25 20:38:35 +0200

    img preload

--- js/main.js
+++ js/main.js
@@ -1,5 +1,13 @@
 $(function () {
 	setFullPage();
+
+	new Image().src="/img/winston-earth.jpg";
+	new Image().src="/img/tracer-close.jpg";
+	new Image().src="/img/widowmaker-close.jpg";
+	new Image().src="/img/solder-close.jpg";
+	new Image().src="/img/mercy-close.jpg";
+	new Image().src="/img/dva-close.jpg";
+
 	var i=1;
 	setInterval(function(){
 		if(i > 6){
